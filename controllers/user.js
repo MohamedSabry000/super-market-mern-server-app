07d017de0358acd6d98888bdf10281118bdaa8d3
SamarNegm <negmsama890@gmail.com>
2022-03-30 18:12:19 +0200

    update and delete

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,15 @@ const User = require('../models/User');
 const { catchAsync } = require('../utils/utils');
 
 module.exports = {
+    findUserByID: catchAsync(async (req, res, next) => {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        if (user === null) {
+            return next({ status: 'failure', message: 'user not found' });
+        }
+        req.user = user;
+        next();
+    }),
     getAllUsers: catchAsync(async (req, res) => {
         const users = await User.find();
         res.json({
@@ -9,6 +18,12 @@ module.exports = {
             data: users,
         });
     }),
+    getUser: async (req, res) => {
+        res.json({
+            status: 'success',
+            data: req.user
+        });
+    },
     createUser: catchAsync(async (req, res) => {
         const { name, email, password } = req.body;
         const user = await User.create({
@@ -30,11 +45,34 @@ module.exports = {
         res.json({ status: 'success', data: user });
     },
     updateUser: async (req, res) => {
-        console.log('updateProduct');
+        const { id } = req.params;
+        try {
+            const user = await User.findByIdAndUpdate(id, req.body, {
+                new: true,
+            });
+
+            res.json({
+                status: 'success',
+                data: user
+            });
+        } catch (error) {
+            res.status(500).json({ status: 'failure', message: err.message });
+        }
     }
     ,
     deleteUser: async (req, res) => {
-        console.log('deleteProduct');
+        const { id } = req.params;
+        try {
+            await User.findByIdAndDelete(id);
+
+
+            res.json({
+                status: 'success',
+                message: "deleted"
+            });
+        } catch (error) {
+            res.status(500).json({ status: 'failure', message: err.message });
+        }
     }
 
 };
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const {
     createUser,
     updateUser,
     deleteUser,
+    getUser,
+    findUserByID,
     uploadAvatar,
 } = require('../controllers/user')
 const router = express.Router();
@@ -13,10 +15,12 @@ const router = express.Router();
 router.get('/login', login);
 router.get('/signup', signup);
 router.get('/', authenticated, getAllUsers);
-
 router.post('/', createUser);
-router.patch('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.use('/:id', authenticated, findUserByID);
+router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+// router.get('/:id', getUser);
+// router.patch('/:id', updateUser);
+// router.delete('/:id', deleteUser);
 // router.patch('/:id/likePost', likePost);
 
 module.exports = router;
